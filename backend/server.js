1ff397cb480a37da0b443ec3bdad8fb3fde54ea7
akshay agnihotri <agnihotriakshay36@gmail.com>
2025-06-22 12:17:27 +0530

    updated .env file

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ app.use(express.json()); // to parse JSON bodies
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on http://localhost:5000");
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
